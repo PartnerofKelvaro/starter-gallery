b010ec1adb69ad7c55b9e2cf29518999a12c3b29
Kelvaro <blizzardkang@gmail.com>
2016-01-24 14:59:57 -0800

    Lab C1

---
+++ application/controllers/About.php
@@ -0,0 +1,19 @@
+<?php
+class About extends CI_Controller{
+    
+    
+ public function about(){
+     
+     $this->load->view('about');
+     
+ }   
+    
+    
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
